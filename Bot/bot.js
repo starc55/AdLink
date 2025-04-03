7347750dc511aea10a54acac993ac07c2ac65ec5
Jony <orziyevogabek67@gmail.com>
2025-04-03 15:38:28 +0500

    my bot

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,10 +2,10 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "7516737132:AAEqJuJM64zfhg8SGDeHcW1qn6CGlfIFu9Y";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://dapper-sorbet-8d662b.netlify.app/";
+const web_link = "https://ad-link.vercel.app/";
 
 bot.start((ctx) =>
-  ctx.reply("Добро пожаловать в наш бот :)))", {
+  ctx.reply("Welcome to the bot. Please feel free to use the bot. :)))", {
     reply_markup: {
       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
     },
